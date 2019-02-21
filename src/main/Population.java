5cf4dceb0b0a22ca00b460dc005dfc8eab955a2d
Eirik Skjaerseth <eirik1502@gmail.com>
2019-02-21 13:44:13 +0100

    implement population

--- src/main/Population.java
+++ src/main/Population.java
@@ -1,9 +1,19 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Population {
+public class Population implements Iterable<Chromosome>{
 
     private List<Chromosome> chromosones = new ArrayList<>();
+
+    public Population(List<Chromosome> chromosomes) {
+
+        this.chromosones = chromosomes;
+    }
+
+    public Iterator<Chromosome> iterator() {
+        return chromosones.iterator();
+    }
 }
