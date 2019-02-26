a102d57a9329e538b2625e675aeff5d39a4125e8
Eirik Skjaerseth <eirik1502@gmail.com>
2019-02-26 13:20:08 +0100

    add drawImageWithSegment

--- src/main/imgseg_representation/IsegImageIO.java
+++ src/main/imgseg_representation/IsegImageIO.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class IsegImageIO {
 
@@ -16,7 +17,7 @@ public class IsegImageIO {
     private static Color[] ColorArray = {Color.black, Color.blue, Color.cyan, Color.darkGray, Color.gray, Color.green, Color.lightGray, Color.magenta, Color.orange, Color.pink, Color.red, Color.white, Color.yellow};
 
 
-    public static imgseg_representation.Image loadImage(String filename) {
+    public static Image loadImage(String filename) {
         BufferedImage buffImg = null;
 
         try {
@@ -25,6 +26,58 @@ public class IsegImageIO {
             System.err.println("could not load image: "+filename);
             return null;
         }
+
+        Image img = convertToImage(buffImg);
+        return img;
+    }
+
+    public static void drawSegmentedImage(Image img, Segmentation seg) {
+        Image segImg = createSegmentedImage(img, seg);
+        drawImage(segImg);
+    }
+
+    public static void drawSegmentation(Segmentation seg) {
+        Image img = createImageOfSegmentation(seg);
+        drawImage(img);
+    }
+
+    public static void drawImage(Image img) {
+        BufferedImage buffImg = convertToBufferedImage(img);
+        drawBufferedImage(buffImg);
+    }
+
+
+    private void drawBuffimgOnAnother(BufferedImage base, BufferedImage another) {
+        Graphics2D g = (Graphics2D) base.getGraphics();
+        g.drawImage(another, 0, 0, null);
+    }
+
+
+
+    private static Image createSegmentedImage(Image img, Segmentation seg) {
+        Image segimg = new Image(img);
+
+        //look at four and four pixels, and make them black if there are different segments within
+        for (int y = 0; y < img.getHeight() -1; y++) {
+            for (int x = 0; x < img.getWidth() -1; x++) {
+                SegLabel label = seg.getLabel(x, y);
+
+                //if the current label has neghbours with different labels, make it black
+                List<SegLabel> neighbours = seg.getNeighbours(label);
+                int diffNeighbourCount = (int) neighbours.stream()
+                        .filter(Objects::nonNull)
+                        .filter(l -> !l.equals(label))
+                        .count();
+                if (diffNeighbourCount != 0) {
+                    Pixel p = segimg.getPixel(x, y);
+                    p.r = p.g = p.b = 0;
+                }
+            }
+        }
+        return segimg;
+    }
+
+    private static Image convertToImage(BufferedImage buffImg) {
 
         Raster raster = buffImg.getRaster(); //the raster object has better pixel obtainment
         int w = raster.getWidth();
@@ -40,17 +93,16 @@ public class IsegImageIO {
                 float[] rgb = raster.getPixel(x,y, (float[])null);
 
                 Pixel p = new Pixel(rgb[0]/rgbMaxVal, rgb[1]/rgbMaxVal, rgb[2]/rgbMaxVal,
-                                    y, x);
+                        y, x);
                 row.add(p);
             }
             pixels.add(row);
         }
 
-        return new imgseg_representation.Image(pixels);
+        return new Image(pixels);
     }
 
-
-    public static void drawSegmentation(Segmentation seg) {
+    private static Image createImageOfSegmentation(Segmentation seg) {
         List<List<Pixel>> pixels = new ArrayList<>(seg.getHeight());
         for (int y = 0; y < seg.getHeight(); y++) {
             List<Pixel> row = new ArrayList<>(seg.getWidth());
@@ -71,23 +123,24 @@ public class IsegImageIO {
             }
             pixels.add(row);
         }
-        imgseg_representation.Image img =  new imgseg_representation.Image(pixels);
-        drawImage(img);
+        return new Image(pixels);
     }
 
-    public static void drawImage(Image img) {
-        //create a buffered image
+    private static BufferedImage convertToBufferedImage(Image img) {
         BufferedImage oimg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
         for (int y = 0; y < img.getHeight(); y++) {
             for (int x = 0; x < img.getWidth(); x++) {
                 setPixel(oimg, x, y, img.getPixel(x, y));
             }
         }
+        return oimg;
+    }
 
+    private static void drawBufferedImage(BufferedImage buffImg) {
         JFrame frame = new JFrame("image");
         frame.setVisible(true);
 
-        frame.add(new JLabel(new ImageIcon(oimg)));
+        frame.add(new JLabel(new ImageIcon(buffImg)));
 
         frame.pack();
 //      frame.setSize(WIDTH, HEIGHT);
