d66a0b4906d59b50b4e10a8b63636325ceaacd5d
eirik1502 <eirik1502@gmail.com>
2019-03-11 12:33:06 +0100

    put objective funcs in chromosome

--- src/main/imgseg_representation/Chromosome.java
+++ src/main/imgseg_representation/Chromosome.java
@@ -1,8 +1,12 @@
 package imgseg_representation;
 
+import imgseg_solver.ChromosomeEvaluations;
+
 public class Chromosome {
     public Segmentation segmentation;
     public Image img;
+
+    public final float overallDev, connectivity;
 
     /**
      * Create an empty chromosome with the size of the given image
@@ -11,6 +15,9 @@ public class Chromosome {
     public Chromosome(Image img){
         this.segmentation = new Segmentation(img);
         this.img = img;
+
+        overallDev = ChromosomeEvaluations.overallDeviation(this);
+        connectivity = ChromosomeEvaluations.connectivity(this);
     }
 
 
