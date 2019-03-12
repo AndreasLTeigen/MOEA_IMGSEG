c04407ce08751d9fb48945c27da35776e6ac033e
AndreasLTeigen <andrealt@stud.ntnu.no>
2019-03-12 14:36:02 +0100

    start uniform crossover function

--- src/main/imgseg_representation/Chromosome.java
+++ src/main/imgseg_representation/Chromosome.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Chromosome {
-    public Segmentation segmentation;
+    public GraphSeg graphSeg;
+    public Segmentation segmentation;  //TODO remove this and fix all other code that relies on this
     public Image img;
 
     public static final int overallDeviationIndex = 0, connectivityIndex = 1;
@@ -26,6 +27,7 @@ public class Chromosome {
      * @param img
      */
     public Chromosome(Image img){
+        //TODO initialize graphSeg
         this.segmentation = new Segmentation(img);
         this.img = img;
     }
---
+++ src/main/solver/uniformCrossover.java
@@ -0,0 +1,10 @@
+package solver;
+
+import imgseg_representation.Chromosome;
+
+public class uniformCrossover {
+
+    public static Chromosome uniformCrossover(Chromosome parent1, Chromosome parent2){
+        for()
+    }
+}
